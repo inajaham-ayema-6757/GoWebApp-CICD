0b7daf4564beb5d9a2308c1d14b521dd54d5897b
inajaham-ayema-6757 <66003930+inajaham-ayema-6757@users.noreply.github.com>
2020-06-26 19:47:18 +0530

    Update myweb.go

--- myweb.go
+++ myweb.go
@@ -4,14 +4,24 @@ import (
 	"fmt"
 	"net"
 	"net/http"
-	"os"
 )
 
+func greeting() string {
+	return "Howdy Folks..xD!!!" 		//Greeting Message
+}
+
+func introduction() string {
+	return "My name is Ameya Makarand Mahajani"		//Intro Message
+}
+
 func helloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "\n\nHowdy Folks..xD!!!\n\n")
-	fmt.Fprintf(w, "My name is Ameya Makarand Mahajani\n")
-	ipAddress, _, _ := net.SplitHostPort(r.RemoteAddr)
-	fmt.Fprintf(w, "IP address is : %s\n\n", ipAddress)
+	fmt.Fprintf(w, "\n\n\n")
+	fmt.Fprintf(w, greeting())
+	fmt.Fprintf(w, "\n")
+	fmt.Fprintf(w, introduction())
+	fmt.Fprintf(w, "\n")
+	IPAddr, _, _ := net.SplitHostPort(r.RemoteAddr)
+	fmt.Fprintf(w, "IP address is : %s", IPAddr)		//Printing Current IP Address
 }
 
 func main() {
@@ -23,11 +33,4 @@ func main() {
 		port = "8080"
 	}
 	http.ListenAndServe(":"+port, nil)
-	//srv := &http.Server{Addr: ":8080"}
-	//go func() {
-	//	srv.ListenAndServe()
-	//}()
-	//time.Sleep(10 * time.Second)
-	//srv.Shutdown(context.TODO())
-	//s.Shutdown(context.Background())
 }
